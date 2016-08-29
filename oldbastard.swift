bf2ab7e687e121c52cbedac0c292bcfc5bf87fb5
Brian <Brian@Memphis.hsd1.tx.comcast.net>
2016-08-29 00:35:50 -0500

    git dun 1

---
+++ oldbastard.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  test 2new
+//
+//  Created by Brian on 8/28/16.
+//  Copyright © 2016 Brian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var youOld: UIImageView!
+    @IBOutlet weak var oldDb: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
